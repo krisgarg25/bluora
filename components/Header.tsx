ab4d26e8b8038f348002f3187fd1e81a87dae969
Kris Garg <krisgarg25@gmail.com>
2025-12-14 03:03:11 +0530

    changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,10 +22,10 @@ export default function Header({ opacity }: HeaderProps) {
     return (
         <motion.header
             style={{ opacity }}
-            className="fixed  w-full z-10 mt-2 "
+            className="fixed  w-full z-1 mt-2 "
         >
             {/* Glass Background Effect */}
-            <div className="absolute w-10xl mx-28 rounded-full inset-0 bg-gradient-to-b from-black/60 via-black/40 to-transparent backdrop-blur-md border  border-white/10"></div>
+            <div className="absolute max-w-full mx-28 rounded-full inset-0 bg-gradient-to-b from-black/60 via-black/40 to-transparent backdrop-blur-md border  border-white/10"></div>
 
             <nav className="relative max-w-7xl mx-auto px-6 lg:px-8 pointer-events-auto">
                 <div className="flex items-center justify-between h-20">
